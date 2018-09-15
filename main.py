90d0c7fdd018cc1da6797a53a94ddf51ef9098a8
Shazz <me@shazz.me>
2018-09-15 06:27:35 +0000

    Change window title

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while(True):
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	cv2.imshow('frame',gray)
+	cv2.imshow('webcam video stream',gray)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
